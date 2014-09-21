dcd9744c720cd4303c33251b35de02d0fd76e84b
dotlouis <1891109+dotlouis@users.noreply.github.com>
2014-09-21 12:35:06 +0200

    Cleaned up functions
    - Removed functions and included the code directly in triggers
    (beforeSave and afterSave) for cleaner code.
    - Now set full name field
    - Also add capitalised nickname and fullname

--- cloud/main.js
+++ cloud/main.js
@@ -1,33 +1,34 @@
 
 var _ = require('underscore');
 
-
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
-});
+String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1).toLowerCase();
+}
 
 Parse.Cloud.beforeSave(Parse.User, function(request, response) {
+
+	// A bit like sudo otherwise the query returns undefined (ACL)
+	Parse.Cloud.useMasterKey();
 
 	var username = request.object.get("username");
 	var email = request.object.get("username");
 
-
 	// Email address based on this format: firstname.lastname@[student.]school.com
 	// Students: simon.wicart@student.france-bs.com
 	// Not student: phillipe.caillot@france-bs.com
+	
+	var namepart = email.substring(0, email.indexOf('@'));
+	var nickname = namepart.substring(0, email.indexOf('.')).capitalize();
+	var fullname = nickname+" "+namepart.substring(email.indexOf('.')+1).capitalize();
 
 	try{
 		// Create the nickname attribute based on email address
-		request.object.set("nickname", email.substring(0, email.indexOf('@')).substring(0, email.indexOf('.')));
+		request.object.set("nickname", nickname);
+		request.object.set("fullname", fullname);
 
 		// Assign school based on email address
 		//request.object.set("school", Parse.User.assignSchool(email));
 		
-		// A bit like sudo otherwise the query returns undefined (ACL)
-		Parse.Cloud.useMasterKey();
-
 		var schoolname;
 
 		// if the email contains @france-bs, assign the student school
@@ -61,62 +62,9 @@ Parse.Cloud.beforeSave(Parse.User, function(request, response) {
 });
 
 Parse.Cloud.afterSave(Parse.User, function(request){
+
+	var user = request.object;
 	// Assign roles based on email adress
-	try{
-		Parse.User.assignRole(request.object);
-	}
-	catch(error){
-		console.log(error);
-	}
-});
-
-Parse.User.makeNickname = function(email){
-	// nickname is first name.
-	return email.substring(0, email.indexOf('@')).substring(0, email.indexOf('.'));
-}
-
-Parse.User.assignSchool = function(user){
-
-	// A bit like sudo otherwise the query returns undefined (ACL)
-	Parse.Cloud.useMasterKey();
-
-	var email = user.get('username');
-	var schoolname;
-
-	// if the email contains @france-bs, assign the student school
-	if(email.substring(email.indexOf('@')).indexOf('france-bs') > -1)
-		schoolname = 'FBS';
-	else
-		schoolname = 'FBS'; // default school
-
-
-	var query = new Parse.Query("School");
-	query.equalTo("shortname", schoolname);
-	query.first ( {
-		success: function(school) {
-			//throw "PLOT TWIST MUFFUGAS";
-			console.log('COME ON !!!!!!');
-
-			// if the school does not already exist throw error
-			// if(_.isUndefined(school)){
-			// 	throw "No school found matching this email address: "+ email;
-			// 	console.log("No school found matching this email address ", email);
-			// }
-			// else{
-				user.set('school', school.id);
-				//user.save();
-				//return school.id;
-			// }
-		},
-		error: function(error) {
-			console.log("SHIT man you piss me off really");
-			throw "[ERROR] " + error.code + " : " + error.message;
-		}
-	});
-
-}
-
-Parse.User.assignRole = function(user){
 
 	// A bit like sudo otherwise the query returns undefined (ACL)
 	Parse.Cloud.useMasterKey();
@@ -132,28 +80,30 @@ Parse.User.assignRole = function(user){
 	else
 		rolename = 'teacher'; //default role
 
+	try{
+		var query = new Parse.Query(Parse.Role);
+		query.equalTo("name", rolename);
+		query.first ( {
+			success: function(role) {
 
-	var query = new Parse.Query(Parse.Role);
-	query.equalTo("name", rolename);
-	query.first ( {
-		success: function(role) {
-
-			// if the role does not already exist create it
-			// if(_.isUndefined(role)){
-			// 	console.log("creating new role: ", rolename);
-			// 	var newRole = new Parse.Role(rolename, new Parse.ACL());
-			// 	newRole.getUsers().add(user);
-			// 	newRole.save();
-			// }
-			// else{
-				role.getUsers().add(user);
-				role.save();
-			// }
-		},
-		error: function(error) {
-			throw "[ERROR] " + error.code + " : " + error.message;
-		}
-	});
-
-}
-
+				// if the role does not already exist create it
+				if(!_.isUndefined(role)){
+					role.getUsers().add(user);
+					role.save();
+				}
+				else{
+					console.log("creating new role: ", rolename);
+					var newRole = new Parse.Role(rolename, new Parse.ACL());
+					newRole.getUsers().add(user);
+					newRole.save();
+				}
+			},
+			error: function(error) {
+				throw "[ERROR] " + error.code + " : " + error.message;
+			}
+		});
+	}
+	catch(error){
+		console.log(error);
+	}
+});
