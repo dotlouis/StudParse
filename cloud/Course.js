6feb75dc16ebad791572df4e086c2ccc101c3852
dotlouis <1891109+dotlouis@users.noreply.github.com>
2014-11-01 11:37:32 +0100

    Changes some relations to pointers
    see
    https://github.com/studapp/Studionic/commit/c9b60e3147e76c0d0575ba5b8c9e
    96cbe89ee8e7

--- cloud/Course.js
+++ cloud/Course.js
@@ -2,12 +2,11 @@ Parse.Cloud.beforeSave("Course", function(request, response) {
 
     Parse.Cloud.useMasterKey();
     var course = request.object;
-    var school_relation = course.relation('school');
 
     try{
-        // Automatically add a relation to FBS
+        // Automatically points to FBS
         new Parse.Query("School").equalTo('nickname','FBS').first().then(function(school){
-            school_relation.add(school);
+            course.set('school', school);
             response.success();
         },function(error){throw error;});
     } catch(error){response.error(error);}
