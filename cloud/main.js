10f7abf599d1546a5b934432b7ed105c7e1fb0ed
dotlouis <1891109+dotlouis@users.noreply.github.com>
2014-11-01 11:32:07 +0100

    forgot mainjs

---
+++ cloud/main.js
@@ -0,0 +1,7 @@
+var _ = require('underscore');
+var tools = require('cloud/tools.js');
+var User = require("cloud/User.js");
+var Role = require("cloud/Role.js");
+var Room = require("cloud/Room.js");
+var Course = require("cloud/Course.js");
+var School = require("cloud/School.js");
